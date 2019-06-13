cff4137754f3cb4edad080d1ce66b0e5a40e0667
Benjy <47220906+Dendridge@users.noreply.github.com>
2019-06-13 11:38:15 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,18 @@
-let pg1, pg2, pta, ta, cv;
-
 function setup(){
     noCanvas();    
-    // Création des éléments HTML
-  
+    // Création des éléments HTML pour un affichage avec bootstrap
+    container = document.getElementById('container');
+    row_header = createDiv().addClass('row');
+    row_content = createDiv().addClass('row');
+    // Label info
+    label_info = createElement('h3', 'Info MarkDown').addClass('col-2');
     // Label tu textarea
-    label_ta = createElement('h3', 'Texte à rédiger en MarkDown');
+    label_ta = createElement('h3', 'Texte à rédiger en MarkDown').addClass('col-5');
     // Label du preview
-    label_preview = createElement('h3', 'Preview après conversion'); 
-    // Textearea view
-    ta = createElement('textarea', 'Texte de base.').addClass('form-control');
-    ta.size(windowWidth/3, windowHeight - 100);
-    // Preview view
-    preview = createP().addClass('preview');
-    
-    // Bouton d'export HTML et wiki
-    btn_html = createButton('Export en HTML').addClass('btn btn-primary');
-    btn_wiki = createButton('Export en Wikimedia').addClass('btn btn-primary');
+    label_preview = createElement('h3', 'Preview après conversion').addClass('col-5'); 
   
     // Label d'information markdown  
-    label_info = createP('MarkDown possible : <br />'
+    div_info = createP('MarkDown possible : <br />'
                         + '<h1>h1 : #</h1>'
                         + '<h2>h2 : ##</h2>'
                         + '<h3>h3 : ###</h3>'
@@ -27,33 +20,38 @@ function setup(){
                         + '<h5>h5 : #####</h5>'
                         + '<h6>h6 : ######</h6>'
                         + '<b>Texte en gras : ** Texte **</b><br />'
-                        + '<i>Texte en italique : * Texte *</i>').addClass('preview');
-    //console.log(pta);
+                        + '<i>Texte en italique : * Texte *</i>').addClass('preview col-2');
+    // Textearea view
+    ta = createElement('textarea', 'Texte de base.').addClass('form-control col-5');
+    // Preview view
+    preview = createP().addClass('preview col-5');
+    // Bouton d'export HTML et wiki
+    btn_html = createButton('Export en HTML').addClass('btn btn-primary');
+    btn_wiki = createButton('Export en Wikimedia').addClass('btn btn-primary');
+  
+    //console.log(ta);
+    // Assignation child > parent
+    row_header.parent(container);
+    row_content.parent(container);
+    label_info.parent(row_header);
+    label_ta.parent(row_header);
+    label_preview.parent(row_header);
+    div_info.parent(row_content);
+    ta.parent(row_content);
+    preview.parent(row_content);
+    btn_html.parent(div_info);
+    btn_wiki.parent(div_info);
+    // Définition d'action des boutons
+    btn_html.mousePressed(export_html);
+    btn_wiki.mousePressed(export_wiki);  
 }
 
 function draw(){
-    // Positionnement des éléments
-    label_ta.position(10,0);
-    label_preview.position(windowWidth/2 + 5, 0);
-    ta.position(10,50);
-    preview.position(windowWidth/2 + 5, 50);
-    preview.size(windowWidth/2 - 25, windowHeight - 100);
-  
-    btn_html.position(ta.x + ta.width + 30, 50);
-    btn_wiki.position(btn_html.x, btn_html.y + 50);
-    btn_html.mousePressed(export_html);
-    btn_wiki.mousePressed(export_wiki);  
-    label_info.position(btn_html.x, btn_wiki.height + btn_wiki.y + 20);
-  
-    // Edition du preview à chaque frappe dans le textarea avec une transformation
+    // A chaque changement mettre à jour le text preview avec transformation
     preview.html(transformation(ta.value()));
 }
-/*
-function windowResized() {
-   resizeCanvas(windowWidth, windowHeight);
-}
-*/
 
+// Fonction permettant la transformation d'élément Markdown en HTML
 function transformation(tx){
     
     // Titre (H1 à H6)
@@ -67,12 +65,11 @@ function transformation(tx){
     
     // Gras + Italique
     // Même chose que pour les titres pour l'ordre
-    tx = tx.replace(/[\*\_]{2}([^\*\_]+)[\*\_]{2}/g, '<b>$1</b>');
-    tx = tx.replace(/[\*\_]{1}([^\*\_]+)[\*\_]{1}/g, '<i>$1</i>');
+    tx = tx.replace(/[\*]{2}([^\*]+)[\*]{2}/g, '<b>$1</b>');
+    tx = tx.replace(/[\*]{1}([^\*]+)[\*]{1}/g, '<i>$1</i>');
     
     
-    // Saut à la ligne (P)
-    
+    // Saut à la ligne (P)    
     tx = tx.replace(/^\s*(\n)?(.+)/gm, function(m){
       return  /\<(\/)?(h\d)/.test(m) ? m : '<p>'+m+'</p>';
     });
@@ -80,11 +77,14 @@ function transformation(tx){
     return tx;
     
   }
-
+  // Fonction d'export du format HTML
   function export_html(){
     console.log(preview.elt.innerHTML);
+    saveStrings(split(preview.elt.innerHTML, ' '), 'version_html.txt');
   }
 
+  // Fonction d'export du format Wiki
   function export_wiki(){
     console.log(ta.elt.value);
+    saveStrings(split(ta.elt.value, ' '), 'version_wiki.txt');
   }
